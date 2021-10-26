24541839ad5e8ad7375905cde7186f7fa193d6d3
Груша Илья <ilya.grusha591@gmail.com>
2021-10-26 14:36:54 +0300

    Добавил несколько страниц, заголовки, изменил некоторые кнопки.

---
+++ Clinics/Pages/MedicalCardPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clinics.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MedicalCardPage.xaml
+    /// </summary>
+    public partial class MedicalCardPage : Page
+    {
+        public MedicalCardPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrescript_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnConclusion_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnTie_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
